zwisus/TrinityCore-3.3.5-with-NPCBots

commit a34e28a9a38569519e82a566a846fe11e644d19c

NPCBots: tempfix for disappearing 2H off-hands (https://github.com/trickerer/Trinity-Bots/issues/170)

src/server/game/AI/NpcBots/bot_ai.cpp
@@ -7211,6 +7211,7 @@ bool bot_ai::OnGossipSelect(Player* player, Creature* creature/* == me*/, uint32
         {
             if (Aura* trans = me->AddAura(MODEL_TRANSITION, me))
             {
+                me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, 0); //debug: remove offhand visuals
                 trans->SetDuration(500);
                 trans->SetMaxDuration(500);
             }
@@ -14713,6 +14714,19 @@ bool bot_ai::GlobalUpdate(uint32 diff)
         {
             me->SetFloatValue(UNIT_FIELD_BOUNDINGRADIUS, DEFAULT_PLAYER_BOUNDING_RADIUS * me->GetObjectScale());
             me->SetFloatValue(UNIT_FIELD_COMBATREACH,  DEFAULT_PLAYER_COMBAT_REACH * me->GetObjectScale());
+
+            //debug: restore offhand visual if needed
+            if (me->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1) == 0)
+            {
+                if (CanChangeEquip(BOT_SLOT_OFFHAND) && _equips[BOT_SLOT_OFFHAND])
+                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, _equips[BOT_SLOT_OFFHAND]->GetEntry());
+                else
+                {
+                    int8 id = 1;
+                    EquipmentInfo const* einfo = sObjectMgr->GetEquipmentInfo(me->GetEntry(), id);
+                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, einfo->ItemEntry[BOT_SLOT_OFFHAND]);
+                }
+            }
         }
         //end DEBUG
 

commit 3a800e090e892c26abda53337de309ed8dab76fc

Fix visual bug caused by a34e28a9a3

src/server/game/AI/NpcBots/bot_ai.cpp
@@ -1958,7 +1958,7 @@ void bot_ai::_listAuras(Player const* player, Unit const* unit) const
             << " (" << float(((unit->GetFloatValue(UNIT_FIELD_MINOFFHANDDAMAGE) + unit->GetFloatValue(UNIT_FIELD_MAXOFFHANDDAMAGE)) / 2) / attSpeed) << " " << LocalizedNpcText(player, BOT_TEXT_DPS) << ")";
     }
     if (unit != me ||
-        (me->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2) &&
+        (me->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + BOT_SLOT_RANGED) &&
         _botclass != BOT_CLASS_PALADIN &&
         _botclass != BOT_CLASS_DEATH_KNIGHT &&
         _botclass != BOT_CLASS_DRUID &&
@@ -7215,7 +7215,7 @@ bool bot_ai::OnGossipSelect(Player* player, Creature* creature/* == me*/, uint32
         {
             if (Aura* trans = me->AddAura(MODEL_TRANSITION, me))
             {
-                me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, 0); //debug: remove offhand visuals
+                me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + BOT_SLOT_OFFHAND, 0); //debug: remove offhand visuals
                 trans->SetDuration(500);
                 trans->SetMaxDuration(500);
             }
@@ -14720,15 +14720,15 @@ bool bot_ai::GlobalUpdate(uint32 diff)
             me->SetFloatValue(UNIT_FIELD_COMBATREACH,  DEFAULT_PLAYER_COMBAT_REACH * me->GetObjectScale());
 
             //debug: restore offhand visual if needed
-            if (me->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1) == 0)
+            if (me->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + BOT_SLOT_OFFHAND) == 0 && _canUseOffHand())
             {
                 if (CanChangeEquip(BOT_SLOT_OFFHAND) && _equips[BOT_SLOT_OFFHAND])
-                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, _equips[BOT_SLOT_OFFHAND]->GetEntry());
+                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + BOT_SLOT_OFFHAND, _equips[BOT_SLOT_OFFHAND]->GetEntry());
                 else
                 {
                     int8 id = 1;
                     EquipmentInfo const* einfo = sObjectMgr->GetEquipmentInfo(me->GetEntry(), id);
-                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, einfo->ItemEntry[BOT_SLOT_OFFHAND]);
+                    me->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + BOT_SLOT_OFFHAND, einfo->ItemEntry[BOT_SLOT_OFFHAND]);
                 }
             }
         }